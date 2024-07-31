26bb3d5266e622b19d53428433b31df4d5bc7cda
Kamil Jarosz <kjarosh256@gmail.com>
2024-07-31 09:45:43 +0200

    text: Reset max font size on newline
    Before this patch, max font size (which claimed to be the highest font
    size observed within the current line) was calculated for each span,
    regardless of newlines.

--- core/src/html/layout.rs
+++ core/src/html/layout.rs
@@ -417,6 +417,7 @@ impl<'a, 'gc> LayoutContext<'a, 'gc> {
 
         self.is_first_line = true;
         self.has_line_break = true;
+        self.max_font_size = Twips::from_pixels(self.current_line_span.font.size);
     }
 
     /// Adjust the text layout cursor down to the next line.
@@ -445,6 +446,7 @@ impl<'a, 'gc> LayoutContext<'a, 'gc> {
 
         self.is_first_line = false;
         self.has_line_break = true;
+        self.max_font_size = Twips::from_pixels(self.current_line_span.font.size);
     }
 
     /// Adjust the text layout cursor in response to a tab.
