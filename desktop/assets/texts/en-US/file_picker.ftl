4971168aba52bcf671151ea3e5fd15175fbb792a
Kamil Jarosz <kjarosh256@gmail.com>
2024-08-06 16:46:59 +0200

    desktop: Add pick_directory

--- desktop/assets/texts/en-US/file_picker.ftl
+++ desktop/assets/texts/en-US/file_picker.ftl
@@ -1,4 +1,5 @@
 file-picker-title-open-file = Open a single file
+file-picker-title-open-directory = Open a folder
 file-picker-filter-supported = All Supported Files
 file-picker-filter-swf = SWF (*.swf)
 file-picker-filter-spl = FutureSplash Animator (*.spl)
--- desktop/src/gui/picker.rs
+++ desktop/src/gui/picker.rs
@@ -1,6 +1,14 @@
-use super::text;
-use crate::preferences::GlobalPreferences;
+use super::{text, LocalizableText};
+use crate::{
+    custom_event::RuffleEvent,
+    gui::{
+        dialogs::pick_path_dialog::{PickPathDialogConfiguration, PickPathDialogResult},
+        DialogDescriptor,
+    },
+    preferences::GlobalPreferences,
+};
 use rfd::AsyncFileDialog;
+use ruffle_frontend_utils::{bundle::Bundle, content::PlayingContent};
 use std::{
     path::PathBuf,
     sync::{
@@ -8,7 +16,10 @@ use std::{
         Arc, Weak,
     },
 };
-use winit::window::Window;
+use tokio::sync::oneshot;
+use url::Url;
+use walkdir::{DirEntry, WalkDir};
+use winit::{event_loop::EventLoopProxy, window::Window};
 
 #[derive(Clone)]
 pub struct FilePicker {
@@ -16,15 +27,21 @@ pub struct FilePicker {
 }
 
 struct FilePickerData {
+    event_loop: EventLoopProxy<RuffleEvent>,
     parent: Weak<Window>,
     picking: AtomicBool,
     preferences: GlobalPreferences,
 }
 
 impl FilePicker {
-    pub fn new(parent: Weak<Window>, preferences: GlobalPreferences) -> Self {
+    pub fn new(
+        parent: Weak<Window>,
+        preferences: GlobalPreferences,
+        event_loop: EventLoopProxy<RuffleEvent>,
+    ) -> Self {
         Self {
             data: Arc::new(FilePickerData {
+                event_loop,
                 parent,
                 picking: AtomicBool::new(false),
                 preferences,
@@ -54,6 +71,76 @@ impl FilePicker {
         } else {
             None
         }
+    }
+
+    pub async fn pick_ruffle_directory(&self, dir: Option<PathBuf>) -> Option<PlayingContent> {
+        let locale = &self.data.preferences.language();
+        let mut dialog =
+            AsyncFileDialog::new().set_title(text(locale, "file-picker-title-open-directory"));
+
+        if let Some(ref dir) = dir {
+            dialog = dialog.set_directory(dir);
+        }
+
+        let directory: PathBuf = self.show_dialog(dialog, |d| d.pick_folder())?.await?.into();
+
+        // Try opening a bundle...
+        match Bundle::from_path(&directory) {
+            Ok(bundle) => {
+                let url = bundle.information().url.clone();
+                return Some(PlayingContent::Bundle(url, Box::new(bundle)));
+            }
+            Err(err) => {
+                tracing::info!("The opened directory is not a bundle: {err}");
+            }
+        }
+
+        // If there's only one SWF in the directory, treat is as the root...
+        let mut files: Vec<DirEntry> = WalkDir::new(&directory)
+            .into_iter()
+            .filter_map(Result::ok)
+            .filter(|e| e.file_type().is_file())
+            .filter(|e| e.file_name().as_encoded_bytes().ends_with(b".swf"))
+            .take(2)
+            .collect();
+        if files.len() == 1 {
+            let file = files.remove(0);
+            if let Ok(url) = Url::from_file_path(file.path()) {
+                tracing::info!(
+                    "There's only one .swf file in the directory, treating it as the root: {}",
+                    url
+                );
+                return Some(PlayingContent::DirectFile(url));
+            }
+        }
+
+        let (notifier, receiver) = oneshot::channel();
+        let _ =
+            self.data
+                .event_loop
+                .send_event(RuffleEvent::OpenDialog(DialogDescriptor::PickPath(
+                    PickPathDialogConfiguration::new(
+                        notifier,
+                        directory.clone(),
+                        LocalizableText::LocalizedText("select-root-movie-title"),
+                        Some(LocalizableText::LocalizedText(
+                            "select-root-movie-message",
+                        )),
+                        LocalizableText::LocalizedText("select-root-movie-pick"),
+                    ),
+                )));
+        let PickPathDialogResult::PickPath(root_movie) = receiver.await.ok()? else {
+            return None;
+        };
+
+        if !root_movie.starts_with(&directory) {
+            // TODO safety check, do we need it?
+            return None;
+        }
+
+        Url::from_file_path(root_movie)
+            .ok()
+            .map(PlayingContent::DirectFile)
     }
 
     pub fn show_dialog<F, O>(&self, mut dialog: AsyncFileDialog, f: F) -> Option<O>
